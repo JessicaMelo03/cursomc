e42cc6799208b9c30f4125c2a74f7b4a34ed3bad
JessicaMelo03 <jmeloferreira3@gmail.com>
2017-10-09 19:07:50 -0300

    Ajustes endpoints

--- src/main/java/com/jessicamelo/cursomc/services/CategoriaService.java
+++ src/main/java/com/jessicamelo/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.jessicamelo.cursomc.domain.Categoria;
 import com.jessicamelo.cursomc.repositories.CategoriaRepository;
+import com.jessicamelo.cursomc.services.exeption.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,11 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado Id:" + id +
+					",Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
