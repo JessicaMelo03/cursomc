9e7d7333a0ae1b6dea9eccc12c1f34de3e9fa841
JessicaMelo03 <jmeloferreira3@gmail.com>
2017-10-09 17:41:26 -0300

    Criação da classe de dominio - Produto, relação muitos pra muitos -Produto e Categoria

---
+++ src/main/java/com/jessicamelo/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.jessicamelo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jessicamelo.cursomc.domain.Produto;
+
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
