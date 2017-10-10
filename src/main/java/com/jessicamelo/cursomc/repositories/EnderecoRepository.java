f9cf66cc4b08101a18e9dfcfc13fe68c1a0b7cb2
JessicaMelo03 <jmeloferreira3@gmail.com>
2017-10-10 10:16:49 -0300

    Cliente, tipo cliente, endereços e telefone

---
+++ src/main/java/com/jessicamelo/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,13 @@
+package com.jessicamelo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jessicamelo.cursomc.domain.Endereco;
+
+
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
